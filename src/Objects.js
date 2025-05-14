b1f32e2e7311b62767cced15a33cd2097dfb2c6d
Surikova Elena <surikovaelena_1997@mail.ru>
2025-05-14 21:18:33 +0300

    задание по объектам

--- src/Objects.js
+++ src/Objects.js
@@ -8,13 +8,35 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+  if (data) {
+    if (data.gender && data.gender == 'female') {
+      if (data.age) {
+        delete data.age;
+      }
+    } else if ( data.gender && data.gender == 'male') {
+      if (!data.income) {
+        data.income = 100000; }
+    }
+    return data;
+  }  
+  else {
+    return "Объект не найден";
+  }
+
 }
+
 
 /*
   В функцию objectFieldsList приходят три объекта с различными полями, список которых заранее неизвестен.
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+  let keys1 = Object.keys(obj1),
+      keys2 = Object.keys(obj2),
+      keys3 = Object.keys(obj3); 
+  let keys = [...keys1, ...keys2, ...keys3].sort();
+  let uniqueKeys = keys.filter((key, index) => (keys.indexOf(key) == index));
+  return uniqueKeys;
 }
 
 /*
@@ -23,4 +45,13 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+let arrayCloneObject = [];
+for (let i = 0; i < count; i++){
+  arrayCloneObject[i] = { id: i, ...obj};
 }
+return arrayCloneObject; 
+}
+
+
+
+
